8a6866167ed1b80a8914560fcc22c145e86a1351
Neamane <nehela08@gmail.com>
2025-09-26 00:12:03 +0200

    Cambios temporales

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,19 @@
-const toggleBtn = document.querySelector("#theme-toggle");
-
-toggleBtn.addEventListener("click", () => {
-    document.documentElement.classList.toggle("dark");
+// Dark Mode Desktop
+const toggleDesktop = document.querySelector("#theme-toggle-desktop");
+toggleDesktop.addEventListener("click", () => {
+  document.documentElement.classList.toggle("dark");
 });
+
+// Dark Mode Mobile
+const toggleMobile = document.querySelector("#theme-toggle-mobile");
+toggleMobile.addEventListener("click", () => {
+  document.documentElement.classList.toggle("dark");
+});
+
+// Toggle menú móvil
+const mobileBtn = document.querySelector("#mobile-menu-btn");
+const mobileMenu = document.querySelector("#mobile-menu");
+mobileBtn.addEventListener("click", () => {
+  mobileMenu.classList.toggle("hidden");
+});
+
