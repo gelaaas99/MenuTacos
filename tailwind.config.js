536ffc3e6fd135b8d97306fd535e0ee61a980faf
Neamane <nehela08@gmail.com>
2025-09-28 23:23:04 +0200

    He retocado el header, y aparte he metido un inicio que irá mejorando.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./*.html", "./assets/js/**/*.js"],
-    darkMode: "class",
-    theme: {
-      extend: {},
+  content: ["./*.html", "./assets/js/**/*.js"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      boxShadow: {
+        'light': '0 4px 6px rgba(255, 248, 240, 0.1)', // sombra clara para modo oscuro
+      }
     },
-    plugins: [],
+  },
+  plugins: [],
 }
 
